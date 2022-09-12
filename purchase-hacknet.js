1238b01ba40918c8de146625e5fdef7421c1d686
Mingyu Kwon <mg_kwon@naver.com>
2022-09-13 01:14:08 +0900

    hacknet이 없을 때 1개 구입

--- purchase-hacknet.js
+++ purchase-hacknet.js
@@ -2,6 +2,14 @@
 export async function main(ns) {
     while (true) {
         var num = ns.hacknet.numNodes();
+        if (num == 0) {
+            if (ns.getServerMoneyAvailable("home") > ns.hacknet.getPurchaseNodeCost()) {
+                ns.hacknet.purchaseNode();
+            }
+            await ns.sleep(1000);
+            continue;
+        }
+
         for (var i = 0; i < num; i++) {
             while (ns.getServerMoneyAvailable("home") > ns.hacknet.getLevelUpgradeCost(i, 1)) {
                 ns.hacknet.upgradeLevel(i, 1);
@@ -15,7 +23,7 @@ export async function main(ns) {
         }
 
         var stat = ns.hacknet.getNodeStats(num - 1);
-        if (stat.level < 200 || stat.ram < 64 || stat.cores < 16) {
+        if (num % 3 == 0 && (stat.level < 200 || stat.ram < 64 || stat.cores < 16)) {
             await ns.sleep(1000);
         } else {
             if (ns.getServerMoneyAvailable("home") > ns.hacknet.getPurchaseNodeCost()) {
